680b156f532021eb5792fde586333b37e982593a
Richard Coffey <19356161+richdarkcoffee@users.noreply.github.com>
2018-08-06 15:24:42 -0700

    Now the lecture goes on to use a function exported from another file. Here, that's the addNote function called in app.js and exported from notes.js.

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
 
-var user = os.userInfo();
+// var user = os.userInfo();
+
+var res = notes.addNote();
+console.log(res);
 
 // You need to provide a callback function to handle an error if the
 // application is unable to write to the file. Possible write problems
 // include file not created or file/directory permissions
-fs.appendFile('greetings.txt',`Hello ${user.username}! You are ${notes.age}.`, function (err) {
-  if (err) {
-    console.log('Unable to write to file');
-  }
-});
+// fs.appendFile('greetings.txt',`Hello ${user.username}! You are ${notes.age}.`, function (err) {
+//   if (err) {
+//     console.log('Unable to write to file');
+//   }
+// });
 
 // Alternatively, you can use the appendFileSync function if you don't
 // want to throw an error if the file needs to be created
--- notes.js
+++ notes.js
@@ -1,7 +1,8 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = function () {
-    
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'New Note'
 };
 
 console.log(module);
