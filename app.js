7fae5f3ad15da4e328a5a9266a710ecc74020fa1
Richard Coffey <19356161+richdarkcoffee@users.noreply.github.com>
2018-08-08 16:10:12 -0700

    Putting files into the state necessary for section 3 video 13!

--- app.js
+++ app.js
@@ -1,40 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
-
 const _ = require('lodash')
 
-// Here's a simple type checking example from lodash!
-console.log(_.isString(true));
-console.log(_.isString('Richard'));
-
-// Removes duplicates from an array
-var filteredarray = _.uniq(['Richard',1,'Richard',1,2,3,4]);
-console.log(filteredarray);
-
-// var user = os.userInfo();
-
-// var res = notes.addNote();
-// console.log(res);
-
-// How I call notes.add
-// var addnumbers = notes.add(3,4);
-// console.log(addnumbers);
-
-// How the instructor called notes.add
-// console.log('Result: ', notes.add(6,-10));
-
-// You need to provide a callback function to handle an error if the
-// application is unable to write to the file. Possible write problems
-// include file not created or file/directory permissions
-// fs.appendFile('greetings.txt',`Hello ${user.username}! You are ${notes.age}.`, function (err) {
-//   if (err) {
-//     console.log('Unable to write to file');
-//   }
-// });
-
-// Alternatively, you can use the appendFileSync function if you don't
-// want to throw an error if the file needs to be created
-// fs.appendFileSync('greetings.txt','Hello world!');
+const notes = require('./notes.js');
--- notes.js
+++ notes.js
@@ -5,13 +5,4 @@ module.exports.addNote = () => {
     return 'New Note'
 };
 
-module.exports.add = (a, b) => {
-    if (!(isNaN(a)) && !(isNaN(b))) {
-        return a + b;
-    }
-    else {
-        console.log('One or both variables are not numbers.')
-    }
-}
-
 console.log(module);
