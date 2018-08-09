6b9b8a12447cdc83cf3de58c14f0daa169701506
Richard Coffey <19356161+richdarkcoffee@users.noreply.github.com>
2018-08-09 13:57:21 -0700

    Started working with argv in node.js and if else statements. Edited out superfluous console.log statement in notes.js.

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ const fs = require('fs');
 const _ = require('lodash')
 
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+
+if(command === 'add') {
+    console.log('Adding new note.')
+} else if (command === 'list') {
+    console.log('Listing notes.')
+} else if (command === 'read') {
+    console.log('Reading note.')
+} else if (command === 'remove') {
+    console.log('Removing note.')
+}
+else {
+    console.log('Command not recognized.')
+}
+
+
+console.log('Command: ', command);
