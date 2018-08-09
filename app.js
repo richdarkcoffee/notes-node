aa2bf90ff09fb4938776bdae2173bc7610dd48d2
Richard Coffey <19356161+richdarkcoffee@users.noreply.github.com>
2018-08-09 15:17:21 -0700

    Just showing how you can use argv._[] to get command line items from the list.

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ const notes = require('./notes.js');
 
 var argv = yargs.argv;
 var command = process.argv[2];
+// Here we're using process.argv to return the first item passed from the command line
+console.log('Command from process:', command);
+// Alternatively, we can use yargs to get the same information
+command = argv._[0];
 
 // Here we're displaying both the command and yargs information
-console.log('Command:', command);
+console.log('Command from yargs:', command);
 console.log('Yargs:', argv);
 
 
